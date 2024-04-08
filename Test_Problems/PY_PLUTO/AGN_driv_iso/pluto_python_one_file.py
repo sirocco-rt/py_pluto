b4dca10cf6ffab10eabbb5486f576ee020d4c0ba
nscepi <nicolas.scepi@gmail.com>
2024-04-08 10:15:35 +0200

    Added setup for AGN rad driving isothermal run

---
+++ Test_Problems/PY_PLUTO/AGN_driv_iso/pluto_python_one_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import subprocess,sys
+import glob
+from astropy import constants as c
+from astropy import units as u
+from scipy.integrate import quad
+import pyPLUTO as pp
+import numpy as np
+import pluto_python_sub as pps
+import re
+
+pps.pluto2py_rtheta(2476)
+#pps.python_input_file(0,data,cycles=2)
